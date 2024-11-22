9bd91e88d62f86735eb925579b6c1d894dfbe822
Damian <1damiangrubecki1@gmail.com>
2024-11-22 10:17:37 +0100

    Ustawienie time limit

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
 use Chamilo\Kernel;
 use Symfony\Component\HttpFoundation\RedirectResponse as HttpRedirectResponse;
 
+set_time_limit(300);
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
